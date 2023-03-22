6382dc6469b2958df7fd5d6d21f4c029294b1da4
Ribena-dev <devinaa.k19@gmail.com>
2023-03-22 22:38:33 +0800

    can store point without stopping

--- r2waypoint_2.py
+++ r2waypoint_2.py
@@ -34,26 +34,31 @@ class Waypoint(Node):
         self.subscription = self.create_subscription(Odometry,'odom',self.odom_callback,10)
         self.subscription
 
-    def odom_callback(self,msg): 
-        
-        # self.get_logger().info('I heard: "%s"' % pos)
-            cmd_char = str(input(" Press s to store to csv file/ n for new point: "))
-            pos = msg.pose.pose.position 
-            orien = msg.pose.pose.orientation
-            if cmd_char =="n":
-                tb_int = int(input("Enter table number: "))
-                waypoints[tb_int][0].extend((pos.x,pos.y,pos.z))
-                waypoints[tb_int][1].extend((orien.x,orien.y,orien.z,orien.z,orien.w))
+    def odom_callback(self,msg):        
+            
+            self.pos = msg.pose.pose.position 
+            self.orien = msg.pose.pose.orientation
+            # self.get_logger().info('I heard: "%s"' % self.pos)
+            
                 # co[tb_int-1][1].extend((pos.x,pos.y,pos.z))
                 # co[tb_int-1][2].extend((orien.x,orien.y,orien.z,orien.z,orien.w))
-                print(waypoints)
-            if cmd_char == 's': 
-                print("saving...")
-                with open('waypoints.pickle','wb') as handle:
-                    pickle.dump(waypoints, handle, protocol=pickle.HIGHEST_PROTOCOL)
-                # saving_file()
-                
-
+               
+    def waypoint_calling(self):
+        cmd_char = "n"
+        while cmd_char == "n":
+            cmd_char = str(input(" Press s to store to csv file/ n for new point: "))
+        # if cmd_char =="n":
+            rclpy.spin_once(self)
+            # print(self.pos)
+            tb_int = int(input("Enter table number: "))
+            waypoints[tb_int][0].extend((self.pos.x,self.pos.y,self.pos.z))
+            waypoints[tb_int][1].extend((self.orien.x,self.orien.y,self.orien.z,self.orien.z,self.orien.w))
+            # print(waypoints)
+        if cmd_char == 's': 
+            print("saving...")
+            with open('waypoints.pickle','wb') as handle:
+                pickle.dump(waypoints, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            # saving_file()
                  
             # print(pos.x,pos.y,pos.z)
             # print(orien.x,orien.y,orien.z,orien.w)       
@@ -61,10 +66,9 @@ class Waypoint(Node):
 def main(args=None):
     rclpy.init(args=args)
     waypoint = Waypoint()
-    cmd_char = str(input("Press q to get point: "))
-    if cmd_char == "q":
-        rclpy.spin(waypoint)
-
+    # cmd_char = str(input("Press q to get point: "))
+    rclpy.spin_once(waypoint)
+    waypoint.waypoint_calling()
     waypoint.destroy_node()
     rclpy.shutdown()
 
