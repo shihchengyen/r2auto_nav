2a39799a5bc367abf4c1bfd148ee87424f6cdc0c
Wei Rong <chuweiron18@gmail.com>
2023-04-11 14:35:53 +0000

    added bashrc alias

---
+++ .bashrc.ex
@@ -0,0 +1,6 @@
+alias map2base='cd ~/colcon_ws/src/auto_nav/auto_nav && python3 map2base.py'
+alias r2waypoints='cd ~/colcon_ws/src/auto_nav/auto_nav && python3 r2waypoints.py'
+alias tablenav='cd ~/colcon_ws/src/auto_nav/auto_nav && python3 r2table_nav.py'
+export GAZEBO_MODEL_PATH=~/colcon_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models
+source /opt/ros/foxy/setup.bash
+source ~/colcon_ws/install/setup.bash
