63968bb6114e5e4f6ae29784a448c3f44cb4a9e2
yuto1115 <yutown1115@gmail.com>
2024-03-14 20:35:26 +0800

    moved stuffs from eg2310_script

---
+++ mainNode.py
@@ -0,0 +1,222 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Pose
+from rclpy.qos import qos_profile_sensor_data
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import OccupancyGrid
+from tools.angleConversion import euler_from_quaternion
+import numpy as np
+import math
+import cmath
+
+
+# constants
+rotatechange = 0.1
+
+class MainEG2310(Node):
+    def __init__(self):
+        super().__init__('mainNode')
+
+        # ''' ================================================ http request ================================================ '''
+        # # Create a subscriber to the topic "doorStatus"
+        # # Listens for the doorStatus from the doorRequestNode
+        # self.http_subscription = self.create_subscription(
+        #     String,
+        #     'doorStatus',
+        #     self.http_listener_callback,
+        #     10)
+        # self.http_subscription  # prevent unused variable warning
+        #
+        # # variable to be used to store the doorStatus
+        # self.doorStatus = ""
+        #
+        # # Create a publisher to the topic "doorRequest"
+        # # Publishes the door opening request to the doorRequestNode
+        # self.http_publisher = self.create_publisher(String, 'doorRequest', 10)
+
+        # ''' ================================================ limit switch ================================================ '''
+        # # Create a subscriber to the topic "switchStatus"
+        # # Listens for the switchStatus from the limitSwitchNode
+        # self.switch_subscription = self.create_subscription(
+        #     String,
+        #     'switchStatus',
+        #     self.switch_listener_callback,
+        #     10)
+        # self.switch_subscription  # prevent unused variable warning
+        #
+        # # variable to be used to store the limit switch status
+        # self.switchStatus = ""
+        #
+        # # Create a publisher to the topic "switchRequest"
+        # # Publishes the activate/deacivate request to the limitSwitchNode
+        # self.switch_publisher = self.create_publisher(String, 'switchRequest', 10)
+        #
+        # ''' ================================================ servo control ================================================ '''
+        # # Create a publisher to the topic "servoRequest"
+        # # Publishes the servoRequest to the servoControlNode
+        # self.publisher_ = self.create_publisher(String, 'servoRequest', 10)
+
+        ''' ================================================ TurtleBot control ================================================ '''
+        # Create a publisher for moving TurtleBot
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+
+        ''' ================================================ odometry ================================================ '''
+        # Create a subscriber to the topic "odom"
+        self.odom_subscription = self.create_subscription(
+            Odometry,
+            'odom',
+            self.odom_callback,
+            10)
+        self.odom_subscription  # prevent unused variable warning
+        # initialize variables
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
+
+        ''' ================================================ lidar ================================================ '''
+        # Create a subscriber to the topic "scan"
+        self.scan_subscription = self.create_subscription(
+            LaserScan,
+            'scan',
+            self.scan_callback,
+            qos_profile_sensor_data)
+        self.scan_subscription  # prevent unused variable warning
+        self.laser_range = np.array([])
+
+        ''' ================================================ occupancy map ================================================ '''
+        # Create a subscriber to the topic "map"
+        self.occ_subscription = self.create_subscription(
+            OccupancyGrid,
+            'map',
+            self.occ_callback,
+            qos_profile_sensor_data)
+        self.occ_subscription  # prevent unused variable warning
+        self.occdata = np.array([])
+
+        ''' ================================================ robot position ================================================ '''
+        # Create a subscriber to the topic
+        self.pos_subscription = self.create_subscription(
+            Pose,
+            'position',
+            self.pos_callback,
+            10)
+
+        self.get_logger().info("mainNode has started, bitchesss! >:D")
+
+    def http_listener_callback(self, msg):
+        # "idle", "door1", "door2", "connection error", "http error"
+        self.doorStatus = msg.data
+
+    def switch_listener_callback(self, msg):
+        # "released" or "pressed"
+        self.switchStatus = msg.data
+
+    def odom_callback(self, msg):
+        orientation_quat = msg.pose.pose.orientation
+        self.roll, self.pitch, self.yaw = euler_from_quaternion(orientation_quat.x, orientation_quat.y,
+                                                                orientation_quat.z, orientation_quat.w)
+
+    def scan_callback(self, msg):
+        # create numpy array
+        self.laser_range = np.array(msg.ranges)
+        # print to file
+        # np.savetxt(scanfile, self.laser_range)
+        # replace 0's with nan
+        self.laser_range[self.laser_range == 0] = np.nan
+
+    def occ_callback(self, msg):
+        # create numpy array
+        msgdata = np.array(msg.data)
+        # compute histogram to identify percent of bins with -1
+        # occ_counts = np.histogram(msgdata, occ_bins)
+        # calculate total number of bins
+        # total_bins = msg.info.width * msg.info.height
+        # log the info
+        # self.get_logger().info('Unmapped: %i Unoccupied: %i Occupied: %i Total: %i' % (occ_counts[0][0], occ_counts[0][1], occ_counts[0][2], total_bins))
+
+        # make msgdata go from 0 instead of -1, reshape into 2D
+        oc2 = msgdata + 1
+        # reshape to 2D array using column order
+        # self.occdata = np.uint8(oc2.reshape(msg.info.height,msg.info.width,order='F'))
+        self.occdata = np.uint8(oc2.reshape(msg.info.height, msg.info.width))
+        # print to file
+        # np.savetxt(mapfile, self.occdata)
+
+    def pos_callback(self, msg):
+        self.position.x = msg.position.x
+        self.position.y = msg.position.y
+        self.orientaion = msg.orientation
+        print(self.position.x, self.position.y, self.orientaion)
+
+    def rotatebot(self, rot_angle):
+        # create Twist object
+        twist = Twist()
+
+        # get current yaw angle
+        current_yaw = self.yaw
+        # log the info
+        self.get_logger().info('Current: %f' % math.degrees(current_yaw))
+        # we are going to use complex numbers to avoid problems when the angles go from
+        # 360 to 0, or from -180 to 180
+        c_yaw = complex(math.cos(current_yaw), math.sin(current_yaw))
+        # calculate desired yaw
+        target_yaw = current_yaw + math.radians(rot_angle)
+        # convert to complex notation
+        c_target_yaw = complex(math.cos(target_yaw), math.sin(target_yaw))
+        self.get_logger().info('Desired: %f' % math.degrees(cmath.phase(c_target_yaw)))
+        # divide the two complex numbers to get the change in direction
+        c_change = c_target_yaw / c_yaw
+        # get the sign of the imaginary component to figure out which way we have to turn
+        c_change_dir = np.sign(c_change.imag)
+        # set linear speed to zero so the TurtleBot rotates on the spot
+        twist.linear.x = 0.0
+        # set the direction to rotate
+        twist.angular.z = c_change_dir * rotatechange
+        # start rotation
+        self.publisher_.publish(twist)
+
+        # we will use the c_dir_diff variable to see if we can stop rotating
+        c_dir_diff = c_change_dir
+        # self.get_logger().info('c_change_dir: %f c_dir_diff: %f' % (c_change_dir, c_dir_diff))
+        # if the rotation direction was 1.0, then we will want to stop when the c_dir_diff
+        # becomes -1.0, and vice versa
+        while (c_change_dir * c_dir_diff > 0):
+            # allow the callback functions to run
+            rclpy.spin_once(self)
+            current_yaw = self.yaw
+            # convert the current yaw to complex form
+            c_yaw = complex(math.cos(current_yaw), math.sin(current_yaw))
+            # self.get_logger().info('Current Yaw: %f' % math.degrees(current_yaw))
+            # get difference in angle between current and target
+            c_change = c_target_yaw / c_yaw
+            # get the sign to see if we can stop
+            c_dir_diff = np.sign(c_change.imag)
+            # self.get_logger().info('c_change_dir: %f c_dir_diff: %f' % (c_change_dir, c_dir_diff))
+
+        self.get_logger().info('End Yaw: %f' % math.degrees(current_yaw))
+        # set the rotation speed to 0
+        twist.angular.z = 0.0
+        # stop the rotation
+        self.publisher_.publish(twist)
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    mainEG2310 = MainEG2310()
+
+    try:
+        rclpy.spin(mainEG2310)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        mainEG2310.destroy_node()
+
+
+if __name__ == '__main__':
+    main()
