979125fd449a60f18540950380163364fd4ee642
YTTRIUM398890585 <chaizy073@gmail.com>
2024-03-14 22:34:04 +0800

    tested with gazebo, works well if bucket and bot is not close to wall n room is enclosed

--- bucketFinder/bucketFinderNode.py
+++ bucketFinder/bucketFinderNode.py
@@ -6,8 +6,10 @@ from sensor_msgs.msg import LaserScan
 import numpy as np
 import matplotlib.pyplot as plt
 
+import argparse
+
 class BucketFinderHandler(Node):
-    def __init__(self):
+    def __init__(self, show_plot):
         super().__init__('bucketFinderNode')
         
         # Create a subscriber to the topic "scan"
@@ -30,8 +32,9 @@ class BucketFinderHandler(Node):
         self.range_min = 0.0
         self.range_max = 0.0
         self.range_len = 0
+    
+        self.show_plot = show_plot
         
-        self.get_logger().info("bucketFinderNode has started! :D")
 
     def listener_callback(self, msg):
         # create numpy array to store lidar data
@@ -65,29 +68,61 @@ class BucketFinderHandler(Node):
         # self.get_logger().info("len(self.laser_range): %s" % str(len(self.laser_range)))
         # self.get_logger().info(str(self.index_to_angle(len(self.laser_range), len(self.laser_range))))
         
-        self.drdtheta = self.dr_dtheta(self.laser_range)
-        # self.get_logger().info("len(drdtheta): %s" % str(len(drdtheta)))
-        
-        # Get the indices that would sort the array
-        sorted_indices = np.argsort(self.drdtheta)
+        if all(np.isnan(self.laser_range)):
+            self.get_logger().info("No valid lidar data")
+            self.bucket_angle = np.nan
+            
+        else:
+            self.drdtheta = self.dr_dtheta(self.laser_range)
+            # self.get_logger().info("len(drdtheta): %s" % str(len(drdtheta)))
+            
+            # The last two indices in this array are the indices of the two maximum values
+            self.max_index = np.nanargmax(self.drdtheta)
+            self.min_index = np.nanargmin(self.drdtheta)
+            
+            self.min_angle = self.index_to_angle(self.min_index, len(self.drdtheta))
+            self.max_angle = self.index_to_angle(self.max_index, len(self.drdtheta))
 
-        # The last two indices in this array are the indices of the two maximum values
-        max1_index, max2_index = sorted_indices[-2:]
+            # bucket is in the middle of the two max values, with care taken if it crosses theta = 0, 
+            # returns nan if min and max defer by more than 90 degrees, or less than 10 degress
+            if self.max_angle < 90 and self.min_angle > 270:
+                if abs(self.min_angle - self.max_angle - 360) > 180 or abs(self.min_angle - self.max_angle - 360) < 10:
+                    self.bucket_angle = np.nan
+                else:
+                    temp = (self.min_angle + self.max_angle - 360) // 2
+                    
+                    if temp < 0:
+                        self.bucket_angle = temp + 360
+                    else:
+                        self.bucket_angle = temp
+            else:
+                if abs(self.max_angle - self.min_angle) > 180 or abs(self.max_angle - self.min_angle) < 10:
+                    self.bucket_angle = np.nan
+                else:
+                    self.bucket_angle = (self.min_angle + self.max_angle) // 2
+                
+            
+            # self.bucket_angle = self.index_to_angle(bucket_index, len(self.drdtheta))
+        
+            self.get_logger().info('min_index  : %i' % self.min_index)
+            self.get_logger().info('max_index  : %i' % self.max_index)
+            self.get_logger().info('min_angle  : %i deg' % self.min_angle)
+            self.get_logger().info('max_angle  : %i deg' % self.max_angle)
+            if np.isnan(self.bucket_angle):
+                self.get_logger().info('bucket_angle: nan')
+            else:
+                self.get_logger().info('bucket_angle: %i deg' % self.bucket_angle)
 
-        # bucket is in the middle of the two max values
-        bucket_index = (max1_index + max2_index) // 2
-        
-        bucket_angle = self.index_to_angle(bucket_index, len(self.drdtheta))
-        
-        self.get_logger().info('Bucket: %i deg' % bucket_angle)
-
-        
-        # # find index with minimum value
-        # lr2i = np.nanargmin(self.laser_range)
-        # lr2a = self.index_to_angle(lr2i, self.range_len)
-        # self.get_logger().info('Shortest distance at %i degrees' % lr2a)
+            # # find index with minimum value
+            # lr2i = np.nanargmin(self.laser_range)
+            # lr2a = self.index_to_angle(lr2i, self.range_len)
+            # self.get_logger().info('Shortest distance at %i degrees' % lr2a)
+            
+            if self.show_plot == 'y':
+                self.plot_lidar_readings()
         
     def index_to_angle(self, index, arrLen):
+        # return in degrees
         return (index / (arrLen - 1)) * 359
     
     def dr_dtheta(self, lidarArr):
@@ -96,38 +131,88 @@ class BucketFinderHandler(Node):
         return drdtheta
     
     def plot_lidar_readings(self):
-        self.get_logger().info(str(self.laser_range))
+        # self.get_logger().info(str(self.laser_range))
         
-        # Create an array of angles to match the ranges
-        angles = np.linspace(0, 2*np.pi, len(self.laser_range))
+        # # Create an array of angles to match the ranges
+        # angles = np.linspace(0, 2*np.pi, len(self.laser_range))
 
-        # Create a polar plot
-        plt.figure()
-        ax = plt.subplot(111, projection='polar')
+        # # Create a polar plot
+        # plt.figure()
+        # ax = plt.subplot(111, projection='polar')
     
-        # Plot the laser ranges
-        ax.plot(angles, self.laser_range, 'o', markersize=1, label='Laser Ranges')
+        # # Plot the laser ranges
+        # ax.plot(angles, self.laser_range, 'o', markersize=1, label='Laser Ranges')
 
-        # Plot drdtheta
-        ax.plot(angles, self.drdtheta, 'x', markersize=1, label='drdtheta')
+        # # Plot drdtheta
+        # ax.plot(angles, self.drdtheta, 'x', markersize=1, label='drdtheta')
+        
+        # ax.set_ylim(-1, 1)  # Replace 0 and 10 with the desired minimum and maximum radial limits
 
-        # Add a legend
-        ax.legend()
+
+        # # Add a legend
+        # ax.legend()
 
         # Display the plot
-        plt.show()
+        # plt.show()
+        
+        # plt.figure()
+        # plt.polar(np.arange(0, 360, 360/len(self.laser_range))/180*np.pi, self.laser_range)
+        # plt.title('Laser Ranges')
+
+
+        # plt.figure()
+        # plt.polar(np.arange(0, 360, 360/len(self.drdtheta))/180*np.pi, self.drdtheta)
+        # plt.title('drdtheta')
+        
+        # Clear the figure
+        plt.clf()   
+        
+        # plotting r(theta)
+        plt.polar(np.arange(0, 360, 360/len(self.laser_range))/180*np.pi, self.laser_range, label='laser_range')
+        
+        # plotting dr/dtheta
+        # take absolute to not plot negative value and times 2 to scale is so its visible in one plot with self.laser_range
+        plt.polar(np.arange(0, 360, 360/len(self.drdtheta))/180*np.pi, 2 * np.abs(self.drdtheta), label='drdtheta')
+        
+        # plotting direciton of the bucket
+        line = np.linspace(0, max(self.laser_range), 10)
+        
+        # no need plot line if its nan
+        if not np.isnan(self.bucket_angle):
+            lineAngle = np.empty(10)
+            lineAngle.fill(self.bucket_angle*np.pi/180)
+            plt.polar(lineAngle, line, label='bucket_angle')
+        
+        plt.legend()
+        
+        # pause to make sure the plot gets created
+        plt.pause(0.00000000001)
+
+        # plt.show()
+        
+        
     
 def main(args=None):
     rclpy.init(args=args)
-
-    bucketFinder = BucketFinderHandler()
-
+    
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Start the bucketFinderNode.')
+    parser.add_argument('-s', type=str, default='n', help='Show plot (y/n)')
+    args = parser.parse_args()
+    
+    bucketFinder = BucketFinderHandler(args.s)
+    
+    if args.s == 'y':
+        # create matplotlib figure
+        plt.ion()
+        plt.figure()    
+    
     try:
         rclpy.spin(bucketFinder)
     except KeyboardInterrupt:
         pass
     finally:
-        bucketFinder.plot_lidar_readings()
+        # bucketFinder.plot_lidar_readings()
         bucketFinder.destroy_node()
 
 if __name__ == '__main__':
