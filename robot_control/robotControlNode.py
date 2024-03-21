4119887e4a2098a1117e815badfa0dca5b1e9c78
YTTRIUM398890585 <chaizy073@gmail.com>
2024-03-21 19:27:58 +0800

    modify robot ctrl forward, backward linear and ang vel

--- robot_control/robotControlNode.py
+++ robot_control/robotControlNode.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import UInt8, Float64
+from std_msgs.msg import Int8, Float64, String
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist, Pose
 import numpy as np
@@ -8,8 +8,8 @@ import math
 import cmath
 
 # constants
-linear_speed = 0.1
-angle_speed = 0.5
+max_linear_speed = 0.1
+max_angle_speed = 0.5  # anglularVel is in __???
 
 
 # return the rotation angle around z axis in radians (counterclockwise)
@@ -35,15 +35,22 @@ class RobotControlNode(Node):
 
         ''' ================================================ TurtleBot control ================================================ '''
         # Create a publisher for moving TurtleBot
-        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.publisherFSM = self.create_publisher(Twist, 'cmd_vel', 10)
         publisher_period = 0.1  # seconds
         self.publisherTimer = self.create_timer(publisher_period, self.publisher_callback_fsm)
         
         # publisher_callback_fsm is used as the fsm to control the robot also
-        self.state = "idle"
+        self.state = "rotateStop"
         self.linearVel = 0.0
+        self.angleVel = 0.0
         self.targetAngle = 0
         self.currentYaw = 0
+        
+        ''' ================================================ robotControlNode_state_feedback ================================================ '''
+        # Create a publisher for robotControlNode_state_feedback
+        self.publisherFB = self.create_publisher(String, 'robotControlNode_state_feedback', 10)
+        publisher_period = 0.1  # seconds
+        self.publisherTimer = self.create_timer(publisher_period, self.publisher_callback_state_feedback)
 
         ''' ================================================ odometry ================================================ '''
         # Create a subscriber to the topic "odom"
@@ -57,18 +64,34 @@ class RobotControlNode(Node):
 
         ''' ================================================ cmd_linear ================================================ '''
         # Create a subscriber to the topic "cmd_linear"
+        # -1 to -127    ==> 0% to -100% of the maximum speed
+        # 0             ==> stop
+        # 1 to 127      ==> 0% to 100% of the maximum speed
         self.linear_subscription = self.create_subscription(
-            UInt8,
+            Int8,
             'cmd_linear',
             self.linear_callback,
             10)
 
-        ''' ================================================ cmd_angle ================================================ '''
-        # Create a subscriber to the topic "cmd_angle"
+        ''' ================================================ cmd_anglularVel ================================================ '''
+        # Create a subscriber to the topic "cmd_anglularVel"
+        # -1 to -127    ==> 0% to -100% of the maximum speed, -ve is clockwise
+        # 0             ==> stop
+        # 1 to 127      ==> 0% to 100% of the maximum speed, +ve is anti clockwise
+        self.angle_subscription = self.create_subscription(
+            Int8,
+            'cmd_anglularVel',
+            self.anglularVel_callback,
+            10)
+        
+        ''' ================================================ cmd_deltaAngle ================================================ '''
+        # Create a subscriber to the topic "cmd_deltaAngle"
+        # deltaAngle is in degrees, the angle to rotate by
+        # +ve is anti clockwise, -ve is clockwise
         self.angle_subscription = self.create_subscription(
             Float64,
-            'cmd_angle',
-            self.angle_callback,
+            'cmd_deltaAngle',
+            self.deltaAngle_callback,
             10)
 
         self.get_logger().info("robotControlNode has started! :D")
@@ -78,25 +101,42 @@ class RobotControlNode(Node):
         self.yaw = angle_from_quaternion(orientation_quat.x, orientation_quat.y, orientation_quat.z, orientation_quat.w)
 
     def linear_callback(self, msg):
+        # -1 to -127    ==> 0% to -100% of the maximum speed
+        # 0             ==> stop
+        # 1 to 127      ==> 0% to 100% of the maximum speed
         if msg.data == 0:
             self.linearVel = 0.0
             self.get_logger().info("linear_callback: linear_stop")
-        elif msg.data == 1:
-            self.linearVel = linear_speed
-            self.get_logger().info("linear_callback: linear_forward")
         else:
-            self.get_logger().info("Error: Publication to 'cmd_vel' must be 0 or 1, but got %d" % msg.data)
+            self.linearVel = (msg.data / 127.0) * max_linear_speed
+            self.get_logger().info("linear_callback: with msg %d and speed: %f" % (msg.data, self.linearVel))  
+            
+    def anglularVel_callback(self, msg):
+        # -1 to -127    ==> 0% to -100% of the maximum speed, -ve is clockwise
+        # 0             ==> stop
+        # 1 to 127      ==> 0% to 100% of the maximum speed, +ve is anti clockwise
+        if msg.data == 0:
+            self.state = "rotateStop"
+            self.get_logger().info("anglularVel_callback: rotateStop")
+        else:
+            self.state = "rotateWithVel"
+            self.angleVel = (msg.data / 127.0) * max_angle_speed
+            self.get_logger().info("anglularVel_callback: rotateWithVel with %d and speed: %f" % (msg.data, self.angleVel))
+            
+            # discard anything from deltaAngle_callback
+            self.targetYaw = self.currentYaw
 
-    def angle_callback(self, msg): 
-        # takes in delta angle in degrees
-        # +- 180 cannot work as the direction may change between initial and first iteration, +- 179 seems to work
+    def deltaAngle_callback(self, msg): 
+        # Create a subscriber to the topic "cmd_deltaAngle"
+        # deltaAngle is in degrees, the angle to rotate by
         # +ve is anti clockwise, -ve is clockwise
+        # +- 180 cannot work as the direction may change between initial and first iteration, +- 179 seems to work
         
         if msg.data == 0:
-            self.state = "idle"
-            self.get_logger().info("angle_callback: idle")
+            self.state = "rotateStop"
+            self.get_logger().info("deltaAngle_callback: rotateStop")
         else:
-            self.state = "rotate"
+            self.state = "rotateByAngle"
             self.currentYaw = self.yaw
             self.targetYaw = self.currentYaw + math.radians(msg.data)
             
@@ -111,15 +151,20 @@ class RobotControlNode(Node):
             # get the sign of the imaginary component to figure out which way we have to turn            
             self.initialDirection = np.sign(cmath.phase(self.complexChange)) 
             
-            self.get_logger().info('angle_callback: complexChange: %s' % str(self.complexChange))
-            self.get_logger().info('angle_callback: initialDirection: %f' % self.initialDirection)
-            self.get_logger().info('angle_callback: Current: %f' % math.degrees(self.currentYaw))
-            self.get_logger().info('angle_callback: Desired: %f' % math.degrees(cmath.phase(self.complexTargetYaw)))
+            self.get_logger().info('deltaAngle_callback: complexChange: %s' % str(self.complexChange))
+            self.get_logger().info('deltaAngle_callback: initialDirection: %f' % self.initialDirection)
+            self.get_logger().info('deltaAngle_callback: Current: %f' % math.degrees(self.currentYaw))
+            self.get_logger().info('deltaAngle_callback: Desired: %f' % math.degrees(cmath.phase(self.complexTargetYaw)))
+            
+            # discard anything from anglularVel_callback
+            self.angleVel = 0.0
             
     def rotatebot(self):
-        if self.state == "idle":
+        if self.state == "rotateStop":
             return 0.0
-        elif self.state == "rotate":
+        elif self.state == "rotateWithVel":
+            return self.angleVel
+        elif self.state == "rotateByAngle":
             self.currentYaw = self.yaw
             self.complexCurrentYaw = complex(math.cos(self.currentYaw),math.sin(self.currentYaw))
             self.get_logger().info('angle_callback: Current: %f' % math.degrees(self.currentYaw))
@@ -139,14 +184,17 @@ class RobotControlNode(Node):
                 speedScaling = 1
                 # self.get_logger().info('speedScaling: %f' % speedScaling)
                 # set the direction and speed to rotate
-                return self.directionChange * angle_speed * speedScaling
+                return self.directionChange * max_angle_speed * speedScaling
             else:
                 self.get_logger().info('End Yaw: %f' % math.degrees(self.currentYaw))
-                # set state back to idle
-                self.state = "idle"
+                # set state back to rotateStop
+                self.state = "rotateStop"
                 
                 # return rotation speed to 0
                 return 0.0 
+        else:
+            self.state = "rotateStop"
+            return 0.0
         
     def publisher_callback_fsm(self):
         twist = Twist()
@@ -157,7 +205,13 @@ class RobotControlNode(Node):
         # twist.angular.z is set from the 
         twist.angular.z = self.rotatebot()
             
-        self.publisher_.publish(twist)  
+        self.publisherFSM.publish(twist)  
+    
+    def publisher_callback_state_feedback(self):
+        # only publish the rotate states, linear no feedback
+        msg = String()
+        msg.data = self.state
+        self.publisherFB.publish(msg)
             
 def main(args=None):
     rclpy.init(args=args)
